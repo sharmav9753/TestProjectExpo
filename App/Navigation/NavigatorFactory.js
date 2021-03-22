ff4598043559d6ebfa39f1663bf289485853370f
Vijay@100 <sharmav9753@gmail.com>
2021-03-22 19:54:30 +0530

    Update navigatin header

--- App/Navigation/NavigatorFactory.js
+++ App/Navigation/NavigatorFactory.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 import * as screens from './screens.json';
 import Trading from '../Screens/Trading';
+import * as Colors from '../Constants/Colors';
 
 const Stack = createStackNavigator();
 
 export const NavigatorFactory = () => (
-  <Stack.Navigator headerMode="screen">
+  <Stack.Navigator
+    headerMode="screen"
+    options= {{headerStyles: { backgroundColor: Colors.baseBackground }}}
+  >
     <Stack.Screen
       name={screens.Trading.Name}
       component={Trading}
-      options={{title: screens.Trading.Title}}
+      options={() => ({
+        title: screens.Trading.Title,
+        headerShown: false,
+        headerStyles: { backgroundColor: Colors.baseBackground }
+      })}
     />
   </Stack.Navigator>
 );
