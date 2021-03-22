03760bb50a5e81927c766439c5b15354790a8792
Vijay@100 <sharmav9753@gmail.com>
2021-03-22 19:56:09 +0530

    Create multiple objects to show multiple data

--- App/Network/WS.js
+++ App/Network/WS.js
@@ -1,16 +1,38 @@
 import React from "react";
 import Expo from 'expo';
 
-const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
-
-export const initSocketForChannel = (msg, callback) => {
+export const initTicker = (msg, callback) => {
+  const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
   ws.onopen = () => {
     // connection opened
     console.log('open >>> ');
     ws.send(msg); // send a message
   };
   ws.onmessage = (response) => {
-    console.log(response)
+    callback(response.data);
+  };
+};
+
+export const initTrades = (msg, callback) => {
+  const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+  ws.onopen = () => {
+    // connection opened
+    console.log('open >>> ');
+    ws.send(msg); // send a message
+  };
+  ws.onmessage = (response) => {
+    callback(response.data);
+  };
+};
+
+export const initOrderBook = (msg, callback) => {
+  const ws = new WebSocket('wss://api-pub.bitfinex.com/ws/2');
+  ws.onopen = () => {
+    // connection opened
+    console.log('open >>> ');
+    ws.send(msg); // send a message
+  };
+  ws.onmessage = (response) => {
     callback(response.data);
   };
 };
